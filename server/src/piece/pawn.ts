a4e2e235ce1dcd3576d437cb3ec1d5bd2aec67eb
Ronan <ronanmingon69550@gmail.com>
2022-12-20 06:55:04 +0100

    add pawn

--- server/src/piece/pawn.ts
+++ server/src/piece/pawn.ts
@@ -1,3 +1,22 @@
-export default  class Pawn {
+import Piece from "./piece";
+import {Positon} from "../type";
+
+export default class Pawn implements Piece {
+  can_jump: boolean = false
+  color: "white" | "black" = "black"
+
+  constructor(color: "white" | "black") {
+    this.color = color
+  }
+
+  eat({x, y}: Positon): boolean {
+    return false;
+  }
+
+  move({x, y}: Positon): boolean {
+    return false;
+  }
+
+
 
 }
