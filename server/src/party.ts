67c6e66647ba50395c39a95c9b39305f0ecb9e8f
Ronan <ronanmingon69550@gmail.com>
2022-12-20 06:54:37 +0100

    add instance in base_board

--- server/src/party.ts
+++ server/src/party.ts
@@ -1,21 +1,39 @@
 import {Board} from './type'
+import {WebSocket} from "ws";
+import Pawn from "./piece/pawn";
 
 const BASE_BOARD =
   [
     [],[],[],[],[],[],[],[],
+    [new Pawn('black')],[new Pawn('black')],[new Pawn('black')],[new Pawn('black')],[new Pawn('black')],[new Pawn('black')],[new Pawn('black')],[new Pawn('black')],
     [],[],[],[],[],[],[],[],
     [],[],[],[],[],[],[],[],
     [],[],[],[],[],[],[],[],
     [],[],[],[],[],[],[],[],
-    [],[],[],[],[],[],[],[],
-    [],[],[],[],[],[],[],[],
+    [new Pawn('white')],[new Pawn('white')],[new Pawn('white')],[new Pawn('white')],[new Pawn('white')],[new Pawn('white')],[new Pawn('white')],[new Pawn('white')],
     [],[],[],[],[],[],[],[],
   ]
+
 export class Party {
   board: Board
+  white: WebSocket | undefined
+  black: WebSocket | undefined
 
-  constructor() {
+  constructor(role: "white" | "black", ws: WebSocket, id: string) {
+    role === "white" ? this.setWhite(ws) : this.setBlack(ws)
     this.board = BASE_BOARD
+  }
+
+  setWhite(ws: WebSocket) {
+    this.white = ws
+  }
+
+  setBlack(ws: WebSocket) {
+    this.black = ws
+  }
+
+  join(ws: WebSocket) {
+    this.white ? this.setBlack(ws) : this.setWhite(ws)
   }
 
 }
