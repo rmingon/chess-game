4af725da8f81b7cf6396beb6c4b0eb9009ec21bf
Ronan <ronanmingon69550@gmail.com>
2022-12-20 06:53:20 +0100

    add test ws with vueuse

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+  import {useWebSocket} from "@vueuse/core";
+  import {computed} from "vue";
 
+  const { status, data, send, open, close } = useWebSocket('ws://127.0.0.1:8080')
+
+  computed(() => {
+    if (status.value === "OPEN") {
+      send("YO")
+    }
+  })
 </script>
 
 <template>
-
+ {{ status }}
 </template>
 
 <style scoped>
