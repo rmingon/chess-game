38b7cce95ec46ac6e81905ea3c551ee80563e366
Ronan <ronanmingon69550@gmail.com>
2022-12-20 06:54:52 +0100

    add interface for piece

--- server/src/piece/piece.ts
+++ server/src/piece/piece.ts
@@ -2,6 +2,8 @@ import {Positon} from "../type";
 
 export default interface Piece {
   can_jump: boolean
-  eat: ({x, y}:Positon) => boolean
-  move: ({x, y}:Positon) => boolean
+  color: 'white' | 'black'
+  // constructor(color: 'white' | 'black'): void
+  eat({x, y}:Positon): boolean
+  move({x, y}:Positon): boolean
 }
