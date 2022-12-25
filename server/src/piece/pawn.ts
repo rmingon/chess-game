c3f95ce481c57acf62b7ae2422793383a14e47af
rmingon <ronanmingon69550@gmail.com>
2022-12-25 17:26:56 +0100

    pawn img

--- server/src/piece/pawn.ts
+++ server/src/piece/pawn.ts
@@ -4,9 +4,11 @@ import {Positon} from "../type";
 export default class Pawn implements Piece {
   can_jump: boolean = false
   color: "white" | "black" = "black"
+  img: string = ""
 
   constructor(color: "white" | "black") {
     this.color = color
+    this.img = color === "white" ? 'pawn_white' : 'pawn_black'
   }
 
   eat({x, y}: Positon): boolean {
