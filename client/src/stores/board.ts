99ac5a2ee38e35905c50f3baea93e580860096cb
rmingon <ronanmingon69550@gmail.com>
2022-12-25 17:41:48 +0100

    fix pinia

--- client/src/stores/board.ts
+++ client/src/stores/board.ts
@@ -1,13 +1,18 @@
 import {defineStore} from "pinia";
+import {useWebSocket} from "@vueuse/core";
+import {computed, reactive, ref, toRefs, watch} from "vue";
 
-export const useBoard = defineStore('counter', {
-    state: () => ({ count: 0, name: 'Eduardo' }),
-    getters: {
-        doubleCount: (state) => state.count * 2,
-    },
-    actions: {
-        increment() {
-            this.count++
-        },
-    },
+export const board = defineStore('board', () => {
+    const { status, data, send, open, close } = useWebSocket('ws://127.0.0.1:8080')
+
+    const newGame = (color: string) => {
+        send(JSON.stringify({
+            command: "NEW_GAME",
+            color
+        }))
+    }
+
+    const board = computed(() => JSON.parse(data.value))
+
+    return {status, board, newGame}
 })
