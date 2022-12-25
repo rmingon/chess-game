716cd2a14664b36f78c4f2f9b60c301fd2473b70
rmingon <ronanmingon69550@gmail.com>
2022-12-25 10:48:31 +0100

    add select + button

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,20 +1,29 @@
 <script setup lang="ts">
   import {useWebSocket} from "@vueuse/core";
-  import {computed} from "vue";
+  import {computed, ref} from "vue";
+  import Select from "./atomic/atoms/Select.vue"
+  import Button from "./atomic/atoms/Button.vue";
 
   const { status, data, send, open, close } = useWebSocket('ws://127.0.0.1:8080')
 
-  computed(() => {
-    if (status.value === "OPEN") {
-      send("YO")
-    }
-  })
+  const color_selected = ref<string>("White")
+
+  const setColor = ($event: string) => {
+    color_selected.value = $event
+  }
+
+  const play = () => {
+    send(JSON.stringify({
+      command: "NEW_GAME",
+      color: color_selected.value
+    }))
+  }
+
 </script>
 
 <template>
- {{ status }}
+  <Select :list="['White','Black']" @changed="setColor"/>
+  <Button label="PLAY" @click="play()" />
 </template>
 
-<style scoped>
 
-</style>
