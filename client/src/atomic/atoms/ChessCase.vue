388a49eb12e65e66a7fe41a3eda9f930ba83b45f
rmingon <ronanmingon69550@gmail.com>
2022-12-25 17:27:40 +0100

    add chessCase

--- client/src/atomic/atoms/ChessCase.vue
+++ client/src/atomic/atoms/ChessCase.vue
@@ -1,11 +1,23 @@
 <template>
-
+  <div class="h-24 w-24" :class="[COLOR[props.color]]">
+    <img :src="`@/assets/pieces/${props.piece?.img || 'default'}.png`" alt="">
+  </div>
 </template>
 
 <script setup lang="ts">
 
+  interface Piece {
+    img: string
+  }
+
+  const props = defineProps<{
+    color: 'white' | 'black',
+    img: string
+    piece: Piece | undefined
+  }>()
+
+  const COLOR = {
+    white: "bg-gray-100",
+    black: "bg-gray-400"
+  }
 </script>
-
-<style scoped>
-
-</style>
