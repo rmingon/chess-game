4b8572709f91b7704f23d9603afa68f133624973
rmingon <ronanmingon69550@gmail.com>
2022-12-26 19:49:36 +0100

    fix board

--- server/src/party.ts
+++ server/src/party.ts
@@ -1,17 +1,23 @@
 import {Board} from './type'
 import {WebSocket} from "ws";
 import Pawn from "./piece/pawn";
+import Bishop from "./piece/bishop";
+import Knight from "./piece/knight";
+import Queen from "./piece/queen";
+import King from "./piece/king";
+import Rook from "./piece/rook";
+import Empty from "./piece/empty";
 
 const BASE_BOARD =
   [
-    [[],[],[],[],[],[],[],[]],
-    [[new Pawn('black')],[new Pawn('black')],[new Pawn('black')],[new Pawn('black')],[new Pawn('black')],[new Pawn('black')],[new Pawn('black')],[new Pawn('black')]],
-    [[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[]],
-    [[new Pawn('white')],[new Pawn('white')],[new Pawn('white')],[new Pawn('white')],[new Pawn('white')],[new Pawn('white')],[new Pawn('white')],[new Pawn('white')]],
-    [[],[],[],[],[],[],[],[]],
+    [new Rook('black'),new Knight('black'),new Bishop('black'),new Queen('black'),new King('black'),new Bishop('black'),new Knight('black'),new Rook('black')],
+    [new Pawn('black'),new Pawn('black'),new Pawn('black'),new Pawn('black'),new Pawn('black'),new Pawn('black'),new Pawn('black'),new Pawn('black')],
+    [new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty()],
+    [new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty()],
+    [new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty()],
+    [new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty(),new Empty()],
+    [new Pawn('white'),new Pawn('white'),new Pawn('white'),new Pawn('white'),new Pawn('white'),new Pawn('white'),new Pawn('white'),new Pawn('white')],
+    [new Rook('white'),new Knight('white'),new Bishop('white'),new King('black'),new Queen('white'),new Bishop('white'),new Knight('white'),new Rook('white')],
   ]
 
 export class Party {
