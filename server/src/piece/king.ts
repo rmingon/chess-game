182cc757a8bc1f5cc692ee863a25fa036671eeb8
rmingon <ronanmingon69550@gmail.com>
2022-12-26 19:49:16 +0100

    add king

--- server/src/piece/king.ts
+++ server/src/piece/king.ts
@@ -1,1 +1,21 @@
-export default class King {}
+import Piece from "./piece";
+import {Positon} from "../type";
+import {Basic} from "./basic";
+
+export default class King extends Basic implements Piece {
+
+    constructor(color: "white" | "black") {
+        super()
+        this.color = color
+        this.img = color === "white" ? 'king_white' : 'king_black'
+    }
+
+    eat({x, y}: Positon): boolean {
+        return false;
+    }
+
+    move({x, y}: Positon): boolean {
+        return false;
+    }
+
+}
