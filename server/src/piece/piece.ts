1edc8363e8d6f0d0202530344c5e4b0f7d670f7e
rmingon <ronanmingon69550@gmail.com>
2022-12-26 19:49:55 +0100

    fix piece

--- server/src/piece/piece.ts
+++ server/src/piece/piece.ts
@@ -3,7 +3,7 @@ import {Positon} from "../type";
 export default interface Piece {
   can_jump: boolean
   color: 'white' | 'black'
-  // constructor(color: 'white' | 'black'): void
+  img: string
   eat({x, y}:Positon): boolean
   move({x, y}:Positon): boolean
 }
