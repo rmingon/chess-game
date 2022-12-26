93920d64861aaac890e941a4ca694f1d0b39a73c
rmingon <ronanmingon69550@gmail.com>
2022-12-26 19:50:08 +0100

    a

--- server/src/piece/queen.ts
+++ server/src/piece/queen.ts
@@ -1,1 +1,19 @@
-export default class Queen {}
+import {Positon} from "../type";
+import {Basic} from "./basic";
+import Piece from "./piece";
+
+export default class Queen extends Basic implements Piece {
+    constructor(color: "white" | "black") {
+        super()
+        this.color = color
+        this.img = color === "white" ? 'king_white' : 'king_black'
+    }
+
+    eat({x, y}: Positon): boolean {
+        return false;
+    }
+
+    move({x, y}: Positon): boolean {
+        return false;
+    }
+}
