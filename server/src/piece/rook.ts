2712c2d786d720fc5c9d4a93ece6e57183633f91
rmingon <ronanmingon69550@gmail.com>
2022-12-26 19:50:27 +0100

    add rook

--- server/src/piece/rook.ts
+++ server/src/piece/rook.ts
@@ -1,1 +1,20 @@
-export default class Rook {}
+import {Basic} from "./basic";
+import Piece from "./piece";
+import {Positon} from "../type";
+
+export default class Rook extends Basic implements Piece {
+
+    constructor(color: "white" | "black") {
+        super()
+        this.color = color
+        this.img = color === "white" ? 'king_white' : 'king_black'
+    }
+
+    eat({x, y}: Positon): boolean {
+        return false;
+    }
+
+    move({x, y}: Positon): boolean {
+        return false;
+    }
+}
