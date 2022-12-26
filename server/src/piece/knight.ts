4a66c6c9eea4cf20cbe8f58095a19069a6539774
rmingon <ronanmingon69550@gmail.com>
2022-12-26 19:49:25 +0100

    add knight

--- server/src/piece/knight.ts
+++ server/src/piece/knight.ts
@@ -1,1 +1,22 @@
-export default class Knight {}
+import Piece from "./piece";
+import {Positon} from "../type";
+import {Basic} from "./basic";
+
+export default class Knight extends Basic implements Piece {
+
+    constructor(color: "white" | "black") {
+        super()
+        this.color = color
+        this.img = color === "white" ? 'knight_white' : 'knight_black'
+    }
+
+    eat({x, y}: Positon): boolean {
+        return false;
+    }
+
+    move({x, y}: Positon): boolean {
+        return false;
+    }
+
+
+}
