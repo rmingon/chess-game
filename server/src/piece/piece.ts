cb4e145f9bfe2dc9b5b8b9ce6648a0fdac1c1e8b
rmingon <ronanmingon69550@gmail.com>
2022-12-28 10:10:09 +0100

    add direction function

--- server/src/piece/piece.ts
+++ server/src/piece/piece.ts
@@ -1,9 +1,13 @@
-import {Positon} from "../type";
+import {Position} from "../type";
 
 export default interface Piece {
   can_jump: boolean
   color: 'white' | 'black'
   img: string
-  eat({x, y}:Positon): boolean
-  move({x, y}:Positon): boolean
+  eat({x, y}:Position): boolean
+  move({x, y}:Position): Position[]
+  top({x, y}:Position): Position
+  bottom({x, y}:Position): Position
+  left({x, y}:Position): Position
+  right({x, y}:Position): Position
 }
