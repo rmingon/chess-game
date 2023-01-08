63c1feae24370fc3a630dbf54c58b6bb6b2557ec
Ronan <ronanmingon69550@gmail.com>
2023-01-08 20:44:41 +0100

    add move for king

--- server/src/piece/king.ts
+++ server/src/piece/king.ts
@@ -4,18 +4,31 @@ import {Basic} from "./basic";
 
 export default class King extends Basic implements Piece {
 
-    constructor(color: "white" | "black") {
-        super()
-        this.color = color
-        this.img = color === "white" ? 'king_white' : 'king_black'
-    }
+  constructor(color: "white" | "black") {
+    super()
+    this.color = color
+    this.img = color === "white" ? 'king_white' : 'king_black'
+  }
 
-    eat({x, y}: Position): boolean {
-        return false;
-    }
+  eat({x, y}: Position): boolean {
+    return false;
+  }
 
-    move({x, y}: Position): Position[] {
-        return [];
-    }
+  move({x, y}: Position): Position[] {
+    return []
+  }
+
+  canMoveOn({x, y}: Position): Position[] {
+    return [
+      this.top({x,y}),
+      this.left({x,y}),
+      this.right({x,y}),
+      this.bottom({x,y}),
+      this.topRight({x,y}),
+      this.topLeft({x,y}),
+      this.bottomRight({x,y}),
+      this.bottomLeft({x,y})
+    ];
+  }
 
 }
