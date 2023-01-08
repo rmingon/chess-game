55fb5cb6c0b735f37d4481045c8d932559ab8e57
Ronan <ronanmingon69550@gmail.com>
2023-01-08 20:45:42 +0100

    can move piece + eat + get instance list of eat

--- server/src/party.ts
+++ server/src/party.ts
@@ -7,9 +7,9 @@ import Queen from "./piece/queen";
 import King from "./piece/king";
 import Rook from "./piece/rook";
 import Empty from "./piece/empty";
-import Piece from "./piece/piece";
+import { Piece } from "./type";
 
-const BASE_BOARD =
+const BASE_BOARD : Board =
   [
     [new Rook('black'),new Knight('black'),new Bishop('black'),new King('black'),new Queen('black'),new Bishop('black'),new Knight('black'),new Rook('black')],
     [new Pawn('black'),new Pawn('black'),new Pawn('black'),new Pawn('black'),new Pawn('black'),new Pawn('black'),new Pawn('black'),new Pawn('black')],
@@ -26,7 +26,8 @@ export class Party {
   id: string
   white: WebSocket | undefined
   black: WebSocket | undefined
-  last_selected_piece : Piece | undefined
+  last_selected : Position[] = []
+  last_piece_selected_position : Position = {x: 0, y: 0}
 
   constructor(color: "White" | "Black", ws: WebSocket, id: string) {
     color === "White" ? this.setWhite(ws) : this.setBlack(ws)
@@ -67,26 +68,52 @@ export class Party {
       })
     })
   }
-  getMoveFromLastPiece({x, y}: Position) : Position[] {
-    let can_move_on = this.last_selected_piece?.move({x, y}) || []
-    return can_move_on
-  }
+
+  // TODO refacto this
   selected({x, y}: Position) {
+    if (this.isInLastSelected({x, y}))
+      this.movePiece(this.last_piece_selected_position, {x, y})
     let piece = this.board[x][y]
+    this.last_piece_selected_position = {x, y}
     if (piece instanceof Empty)
       return
-    this.last_selected_piece = piece
+    this.last_selected = []
     this.deselectAll()
     piece.setSelected(true)
     this.setBackground()
-    const can_move_on = piece.move({x,y})
+    const can_move_on = piece.canMoveOn({x,y})
     can_move_on.forEach(({x, y}: Position) => {
-      if(x < 0 || x > 7 || y < 0 || y > 7) // OUTSIDE THE BOARD SO NO INSTANCE OF PIECE
+      if (!this.positionIsInBoard({x, y}))
         return
       const chess_case = this.board[x][y]
-      chess_case.setSelected(true)
+      if (chess_case instanceof Empty || chess_case.color !== piece.color) { // IF CASE ARE EMPTY OR ANOTHER COLOR OF SELECTED PIECE
+        chess_case.setSelected(true)
+        this.last_selected.push({x,y})
+      }
     })
     this.sendBoard()
+  }
+
+  movePiece(from: Position, to: Position) {
+    if (!(this.board[to.x][to.y] instanceof Empty))
+      console.log(this.board[to.x][to.y])
+    this.board[to.x][to.y] = this.board[from.x][from.y]
+    this.board[to.x][to.y].as_already_move = true
+    this.board[from.x][from.y] = new Empty()
+  }
+
+  isInLastSelected({x, y}: Position) : boolean {
+    for (const pos of this.last_selected) {
+      if (pos.x === x && pos.y === y)
+        return true
+    }
+    return false
+  }
+
+  private positionIsInBoard({x, y}: Position) : boolean {
+    if(x < 0 || x > 7 || y < 0 || y > 7) // OUTSIDE THE BOARD SO NO INSTANCE OF PIECE
+      return false
+    return true
   }
 
   private setBackground() {
