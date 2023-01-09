863616e093a6c813ad72b379eeef0f6a4b50dffe
rmingon <ronanmingon69550@gmail.com>
2023-01-09 06:45:09 +0100

    fix indent knight

--- server/src/piece/knight.ts
+++ server/src/piece/knight.ts
@@ -3,31 +3,27 @@ import {Position} from "../type";
 import {Basic} from "./basic";
 
 export default class Knight extends Basic implements Piece {
-    can_jump = true
-
-    constructor(color: "white" | "black") {
-        super()
-        this.color = color
-        this.img = color === "white" ? 'knight_white' : 'knight_black'
-    }
-
-    eat({x, y}: Position): boolean {
-        return false;
-    }
-
+  point = 3
+  can_jump = true
+  constructor(color: "white" | "black") {
+      super()
+      this.color = color
+      this.img = color === "white" ? 'knight_white' : 'knight_black'
+  }
+  eat({x, y}: Position): boolean {
+      return false;
+  }
   canMoveOn({x, y}: Position): Position[] {
-        let b = this.bottom
-        let r = this.right
-        let l = this.left
-        let t = this.top
-        let bottomLine = b(b({x, y}))
-        let rightLine = r(r({x, y}))
-        let leftLine = l(l({x, y}))
-        let topLine = t(t({x, y}))
-        return [r(bottomLine), l(bottomLine), b(rightLine), t(rightLine), b(leftLine), t(leftLine), r(topLine), l(topLine)];
-    }
-
-
+    let b = this.bottom
+    let r = this.right
+    let l = this.left
+    let t = this.top
+    let bottomLine = b(b({x, y}))
+    let rightLine = r(r({x, y}))
+    let leftLine = l(l({x, y}))
+    let topLine = t(t({x, y}))
+    return [r(bottomLine), l(bottomLine), b(rightLine), t(rightLine), b(leftLine), t(leftLine), r(topLine), l(topLine)];
+  }
   move({x, y}: Position): Position[] {
     return []
   }
