7995f945bf7b018958271da1878642a498e51c89
rmingon <rmingon@doing.fr>
2023-01-09 06:33:08 +0100

    add eaten pieces

--- client/src/stores/board.ts
+++ client/src/stores/board.ts
@@ -4,23 +4,30 @@ import {computed, reactive, ref, toRefs, watch} from "vue";
 
 type ws = {
     board: object[],
-    id: string
+    id: string,
+    eaten: object[]
 }
 
 export const board = defineStore('board', () => {
     const { status, data, send, open, close } = useWebSocket('ws://127.0.0.1:8080')
 
-    const newGame = (color: string) => {
-        send(JSON.stringify({
-            command: "NEW_GAME",
-            color
-        }))
-    }
 
     const $ws = computed((): ws => JSON.parse(data.value))
 
     const board = computed(() => $ws.value?.board || [])
     const id = computed(() => $ws.value?.id || "")
+    const eaten_black = computed(() => $ws.value?.eaten.filter(piece => piece.color === "black") || [])
+    const eaten_white = computed(() => $ws.value?.eaten.filter(piece => piece.color === "white") || [])
+
+    const eaten_black_points = computed(() => eaten_black.value.reduce(
+      (acc, piece) => acc + piece.point,
+      0
+    ))
+
+    const eaten_white_points = computed(() => eaten_white.value.reduce(
+      (acc, piece) => acc + piece.point,
+      0
+    ))
 
     const selected = (position: {x: number, y: number}) => {
       send(JSON.stringify({
@@ -30,6 +37,12 @@ export const board = defineStore('board', () => {
           arg: position,
       }))
     }
+    const newGame = (color: string) => {
+        send(JSON.stringify({
+            command: "NEW_GAME",
+            color
+        }))
+    }
 
-    return {status, board, newGame, selected, id}
+    return {status, board, newGame, selected, id, eaten_black, eaten_white, eaten_black_points, eaten_white_points}
 })
